92c9afa6436ae6cf1363630eaae632e99c5a3d5d
Osamavipkill1 <143057796+Osamavipkill1@users.noreply.github.com>
2026-09-05 05:10:18 +0300

    Update Mayhem.lua

--- Mayhem Mode/Mayhem.lua
+++ Mayhem Mode/Mayhem.lua
@@ -182,7 +182,7 @@ if gameId == 6839171747 then
 
     _G.AntiLog = true
 
-Msg("Mayhem Mode - v2.7", 1)
+Msg("Mayhem Mode - v2.8", 1)
 task.wait(2)
 Msg("Made by ThatOneAmethystCreature#0001", 1)
 _G.AntiLog = true
